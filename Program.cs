ca629a4fd0cb84ea414acd78c0016e9d7970e765
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-22 13:54:35 +0530

    Fetched

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Line_Comparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC2_LengthOfLine.Equality_of_Line();
+        }
+           
+    }
+    
+}
